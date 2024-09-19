41220ac7ac12d5c242f6fd18dbc76d44a7a626de
MihhPaula <emilia.paula@fatec.sp.gov.br>
2024-09-19 17:45:18 -0300

    BaseActivity criado, e focando na melhora do layout

--- app/src/main/java/com/example/appfood/BaseActivity.java
+++ app/src/main/java/com/example/appfood/BaseActivity.java
@@ -1,6 +1,7 @@
 package com.example.appfood;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,5 +14,7 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
     }
 }
